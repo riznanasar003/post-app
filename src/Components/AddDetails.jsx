f590e3d4eb0f6668874bac00989d04fdedb5224e
riznanasar003 <richurizna10@gmail.com>
2024-06-08 23:12:27 +0530

    changed successfully

--- src/Components/AddDetails.jsx
+++ src/Components/AddDetails.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavigationBar from './NavigationBar'
 
 const AddDetails = () => {
+    const [data,setData] = useState(
+        {
+
+            "userid":"",
+            "username":"",
+            "emailid":"",
+            "dob":"",
+            "password":"",
+            "confirmpass":"",
+            "feedback":""
+
+        }
+    )
+    const inputHandler = (event) =>{
+        setData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue = () =>{
+        console.log(data)
+    }
   return (
     <div>
             <NavigationBar/>
@@ -13,48 +32,48 @@ const AddDetails = () => {
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                             <label htmlFor="" className="form-label">UserID</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="userid" value={data.userid} onChange={inputHandler}/>
 
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                             <label htmlFor="" className="form-label">Username</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='username' value={data.username} onChange={inputHandler}/>
 
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                             <label htmlFor="" className="form-label">Email Id</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="emailid" value={data.emailid} onChange={inputHandler}/>
 
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                             <label htmlFor="" className="form-label">D.O.B</label>
-                            <input type="date" name="" id="" className="form-control" />
+                            <input type="date" name="dob" value={data.dob} onChange={inputHandler} id="" className="form-control" />
 
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                             <label htmlFor="" className="form-label">Password</label>
-                            <input type="password" name="" id="" className="form-control" />
+                            <input type="password" name="password" value={data.password} onChange={inputHandler} id="" className="form-control" />
 
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                             <label htmlFor="" className="form-label">Confirm Password</label>
-                            <input type="password" name="" id="" className="form-control" />
+                            <input type="password" name="confirmpass" value={data.confirmpass} onChange={inputHandler} id="" className="form-control" />
 
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                             <label htmlFor="" className="form-label">Feedback</label>
-                            <textarea name="" id="" className="form-control"></textarea>
+                            <textarea id="" className="form-control" name="feedback" value={data.feedback} onChange={inputHandler}></textarea>
 
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
-                            <button className="btn btn-dark">POST</button>
+                            <button className="btn btn-dark" onClick={readValue}>POST</button>
 
                         </div>
                     </div>
--- src/Components/DeleteUser.jsx
+++ src/Components/DeleteUser.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavigationBar from './NavigationBar'
 
 const DeleteUser = () => {
+    const [data,setData] = useState(
+        {
+            "username":""
+        }
+    )
+    const inputHandler = (event) =>{
+        setData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue = () =>{
+        console.log(data)
+    }
   return (
     <div>
         <NavigationBar/>
@@ -13,11 +24,11 @@ const DeleteUser = () => {
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                                 <label htmlFor="" className="form-label">Username</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control"  name="username" value={data.username} onChange={inputHandler}/>
                             
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <button className="btn btn-dark">REMOVE</button>
+                            <button className="btn btn-dark" onClick={readValue}>REMOVE</button>
                         </div>
                     </div>
                 </div>
